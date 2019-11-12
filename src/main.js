cf3bc7b7d32be36571ab47c590ba072e2c648008
liqing1013 <936012165@qq.com>
2019-11-12 18:01:49 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui'// 引入组件
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUi)// 注册全局组件
 new Vue({
   router,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+// import about from '../views/About.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,6 +13,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    // component: about
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
