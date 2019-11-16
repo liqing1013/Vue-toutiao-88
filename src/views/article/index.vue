e404619a91714103fd1ce67fb19a759b85f4cdb0
liqing1013 <936012165@qq.com>
2019-11-16 11:23:26 +0800

    文章列表指定条件查询完毕

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -8,18 +8,29 @@
       <!-- 筛选数据卡片内容 -->
       <el-form label-width="80px">
         <el-form-item label="文章状态">
+          <!--
+            单选框组会把选中的radio 的label同步给绑定的数据
+          -->
           <el-radio-group v-model="filterForm.status">
-            <el-radio :label="3">全部</el-radio>
-            <el-radio :label="6">草稿</el-radio>
-            <el-radio :label="9">待审核</el-radio>
-            <el-radio :label="6">审核通过</el-radio>
-            <el-radio :label="9">审核失败</el-radio>
+            <!-- 处理绑定表单中文章状态的单选框组，label -->
+            <el-radio :label="null">全部</el-radio>
+            <el-radio label="0">草稿</el-radio>
+            <el-radio label="1">待审核</el-radio>
+            <el-radio label="2">审核通过</el-radio>
+            <el-radio label="3">审核失败</el-radio>
+            <el-radio label="4">已刪除</el-radio>
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道列表">
+          <!-- 下拉列表会把选中的数据 -->
           <el-select placeholder="请选择频道" v-model="filterForm.channel_id">
-            <el-option label="区域一" value></el-option>
-            <el-option label="区域二" value></el-option>
+            <el-option label="所有频道" :value="null"></el-option>
+            <el-option
+              v-for="channel in channels"
+              :label="channel.name"
+              :value="channel.id"
+              :key="channel.id"
+            ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="时间选择">
@@ -30,8 +41,13 @@
               range-separator="至"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
+              value-format="yyyy-MM-dd"
             ></el-date-picker>
+            <!-- value-format绑定指日期绑定格式，不绑定是日期对象的形式 -->
           </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="loadArticles(1)">查询</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -88,11 +104,9 @@
       background
       layout="prev, pager, next"
       :total="totalCount"
-      @current-change='onPageChange'
+      @current-change="onPageChange"
       :disabled="loading"
-      >
-    </el-pagination>
-
+    ></el-pagination>
   </div>
 </template>
 
@@ -105,27 +119,27 @@ export default {
       // 过滤数据的表单
       filterForm: {
         // 文章状态
-        status: '',
+        status: null,
         // 频道ID
-        channel_id: '',
-        // 起始时间
-        begin_pubdate: '',
-        // 结束时间
-        end_pubdate: ''
+        channel_id: null
+        // // 起始时间
+        // begin_pubdate: '',
+        // // 结束时间
+        // end_pubdate: ''
       },
       // 日期区间范围
-      rangeDate: '',
+      rangeDate: [],
       // 文章列表
       articles: [],
       // 审核状态样式
       Articlestatus: [
         {
           type: '',
-          label: '待审核'
+          label: '草稿'
         },
         {
           type: 'warning',
-          label: '草稿'
+          label: '待审核'
         },
         {
           type: 'success',
@@ -137,18 +151,22 @@ export default {
         },
         {
           type: 'info',
-          label: '已删除'
+          label: '已刪除'
         }
       ],
       // 总记录数
       totalCount: 0,
       // 表格loding状态
-      loading: false
+      loading: true,
+      channels: []
     }
   },
+  // 初始化加载时加载第一页
   created () {
-    // 初始化加载时加载第一页
     this.loadArticles(1)
+
+    // 加载频道列表
+    this.loadChannels()
   },
   methods: {
     // 调用接口，获取后台文章列表数据
@@ -174,26 +192,54 @@ export default {
           // 页码
           page,
           // 每页几条
-          per_page: 15
-
+          per_page: 10,
+          // axios有个功能，当参数为null,undefined时，请求就不发送了
+          status: this.filterForm.status, // 文章状态
+          // channel_id, // 文章状态
+          // begin_pubdate, // 开始时间
+          // end_pubdate// 结束时间
+          // 点击查询时，调用loadArticles这个函数
+          channel_id: this.filterForm.channel_id,
+          // 手动判断数据是否有效，是因为当用户手动清除时，数据被充值为null，因为
+          // 因为数组为null时，选取null[索引]会报错，
+          begin_pubdate: this.rangeDate ? this.rangeDate[0] : null,
+          end_pubdate: this.rangeDate ? this.rangeDate[1] : null
         }
-      }).then(res => {
-        console.log(res)
-        // 跟新文章列表
-        this.articles = res.data.data.results
-        // 更新文章记录总数
-        this.totalCount = res.data.data.total_count
-      }).catch(res => {
-        console.log(res, '获取数据失败')
-      }).finally(() => {
-        // 无论成功还是失败，最终都要执行
-        this.loading = false
       })
+        .then(res => {
+          console.log(res)
+          // 跟新文章列表
+          this.articles = res.data.data.results
+          // 更新文章记录总数
+          this.totalCount = res.data.data.total_count
+        })
+        .catch(res => {
+          console.log(res, '获取数据失败')
+        })
+        .finally(() => {
+          // 无论成功还是失败，最终都要执行
+          this.loading = false
+        })
     },
     // 页码改变时调用此函数
     onPageChange (page) {
       // 请求加载指定页码的文章列表
       this.loadArticles(page)
+    },
+    // 获取服务器频道数据
+    loadChannels () {
+      // 有些接口不需要token
+      this.$axios({
+        method: 'GET',
+        url: '/channels'
+      })
+        .then(res => {
+          console.log(res)
+          this.channels = res.data.data.channels
+        })
+        .catch(res => {
+          console.log(res)
+        })
     }
   }
 }
